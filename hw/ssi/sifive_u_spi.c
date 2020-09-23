950757f3ef8d14603e112498b854c863013947f4
Pragnesh Patel <pragnesh.patel@sifive.com>
2020-09-23 17:02:50 +0530

    ssi: Add SiFive SPI Controller
    Signed-off-by: Pragnesh Patel <pragnesh.patel@sifive.com>

---
+++ hw/ssi/sifive_u_spi.c
@@ -0,0 +1,188 @@
+/*
+ * Arm PrimeCell PL022 Synchronous Serial Port
+ *
+ * Copyright (c) 2020 SiFIve,Inc
+ * Written by Pragnesh Patel
+ *
+ * This code is licensed under the GPL.
+ */
+
+#include "qemu/osdep.h"
+#include "hw/sysbus.h"
+#include "migration/vmstate.h"
+#include "hw/irq.h"
+#include "hw/ssi/ssi.h"
+#include "qemu/log.h"
+#include "qemu/module.h"
+
+#include "hw/riscv/sifive_u_spi.h"
+#include "hw/qdev-properties.h"
+
+//#define SIFIVE_SPI_ERR_DEBUG 1
+
+#ifdef SIFIVE_SPI_ERR_DEBUG
+#define DB_PRINT(...) do { \
+    fprintf(stderr,  ": %s: ", __func__); \
+    fprintf(stderr, ## __VA_ARGS__); \
+    } while (0)
+#else
+    #define DB_PRINT(...)
+#endif
+
+
+static void sifive_spi_transfer(sifiveSPI *s)
+{
+
+    s->regs[R_RXDATA] = ssi_transfer(s->ssi, s->regs[R_TXDATA]);
+
+}
+
+static uint64_t
+spi_read(void *opaque, hwaddr addr, unsigned int size)
+{
+    sifiveSPI *s = opaque;
+    uint32_t r = 0;
+
+    addr >>= 2;
+    switch (addr) {
+    case R_RXDATA:
+//	s->regs[R_IP] |= SIFIVE_SPI_IP_RXWM; 
+        r = s->regs[R_RXDATA];
+    	DB_PRINT("Data received: 0x%x\n", s->regs[R_RXDATA]);
+        break;
+    case R_IP:
+	s->regs[R_IP] |= SIFIVE_SPI_IP_RXWM;
+	r = s->regs[R_IP];
+	break;
+    default:
+        if (addr < ARRAY_SIZE(s->regs)) {
+            r = s->regs[addr];
+        }
+        break;
+    }
+
+    DB_PRINT("addr=" TARGET_FMT_plx " = %x\n", addr * 4, r);
+    return r;
+}
+
+static void
+spi_write(void *opaque, hwaddr addr,
+            uint64_t val64, unsigned int size)
+{
+    sifiveSPI *s = opaque;
+    uint32_t value = val64;
+
+    DB_PRINT("addr=" TARGET_FMT_plx " = %x\n", addr, value);
+    addr >>= 2;
+    switch (addr) {
+    case R_TXDATA:
+	s->regs[R_TXDATA] = (unsigned)value;
+    	DB_PRINT("Data to send: 0x%x\n", s->regs[R_TXDATA]);
+        sifive_spi_transfer(s);
+	s->regs[R_IP] |= SIFIVE_SPI_IP_TXWM;
+	break;
+    default:
+        if (addr < ARRAY_SIZE(s->regs)) {
+            s->regs[addr] = value;
+        }
+        break;
+    }
+}
+
+static void sifive_spi_do_reset(sifiveSPI *s)
+{
+    memset(s->regs, 0, sizeof s->regs);
+
+    s->regs[R_TXMARK] = 1;
+    s->regs[R_CSDEF] = 1;
+    s->regs[R_IP] = (SIFIVE_SPI_IP_TXWM | SIFIVE_SPI_IP_RXWM);
+}
+
+static void sifive_spi_reset(DeviceState *d)
+{
+    sifive_spi_do_reset(SIFIVE_SPI(d));
+}
+
+static const MemoryRegionOps spi_ops = {
+    .read = spi_read,
+    .write = spi_write,
+    .endianness = DEVICE_NATIVE_ENDIAN,
+};
+
+#if 0
+static int pl022_post_load(void *opaque, int version_id)
+{
+    PL022State *s = opaque;
+
+    if (s->tx_fifo_head < 0 ||
+        s->tx_fifo_head >= ARRAY_SIZE(s->tx_fifo) ||
+        s->rx_fifo_head < 0 ||
+        s->rx_fifo_head >= ARRAY_SIZE(s->rx_fifo)) {
+        return -1;
+    }
+    return 0;
+}
+#endif
+
+static const VMStateDescription vmstate_sifive_spi = {
+    .name = "sifive_spi",
+    .version_id = 1,
+    .minimum_version_id = 1,
+    .fields = (VMStateField[]) {
+        VMSTATE_UINT32_ARRAY(regs, sifiveSPI, R_MAX),
+        VMSTATE_END_OF_LIST()
+    }
+};
+
+static void sifive_spi_realize(DeviceState *dev, Error **errp)
+{
+    SysBusDevice *sbd = SYS_BUS_DEVICE(dev);
+    sifiveSPI *s = SIFIVE_SPI(dev);
+    //int i;
+
+    DB_PRINT("\n");
+
+    s->ssi = ssi_create_bus(dev, "ssi");
+
+    sysbus_init_irq(sbd, &s->irq);
+
+#if 0
+    s->cs_lines = g_new0(qemu_irq, s->num_cs);
+    for (i = 0; i < s->num_cs; ++i) {
+        sysbus_init_irq(sbd, &s->cs_lines[i]);
+    }
+#endif
+
+    memory_region_init_io(&s->mmio, OBJECT(s), &spi_ops, s,
+                          "sifive-spi", R_MAX * 4);
+    sysbus_init_mmio(sbd, &s->mmio);
+}
+
+static Property sifive_spi_properties[] = {
+    DEFINE_PROP_UINT8("num-ss-bits", sifiveSPI, num_cs, 1),
+    DEFINE_PROP_END_OF_LIST(),
+};
+
+static void sifive_spi_class_init(ObjectClass *klass, void *data)
+{
+    DeviceClass *dc = DEVICE_CLASS(klass);
+
+    dc->realize = sifive_spi_realize;
+    dc->reset = sifive_spi_reset;
+    device_class_set_props(dc, sifive_spi_properties);
+    dc->vmsd = &vmstate_sifive_spi;
+}
+
+static const TypeInfo sifive_spi_info = {
+    .name           = TYPE_SIFIVE_SPI,
+    .parent         = TYPE_SYS_BUS_DEVICE,
+    .instance_size  = sizeof(sifiveSPI),
+    .class_init     = sifive_spi_class_init,
+};
+
+static void sifive_spi_register_types(void)
+{
+    type_register_static(&sifive_spi_info);
+}
+
+type_init(sifive_spi_register_types)
---
+++ include/hw/riscv/sifive_u_spi.h
@@ -0,0 +1,67 @@
+/*
+ * QEMU SiFive System-on-Chip SPI register definition
+ *
+ * Copyright 2020 SiFive,Inc
+ *
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms and conditions of the GNU General Public License,
+ * version 2 or later, as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
+ * more details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef SIFIVE_SPI_H
+#define SIFIVE_SPI_H
+
+#include "hw/sysbus.h"
+
+#define TYPE_SIFIVE_SPI "sifive_soc.spi"
+#define SIFIVE_SPI(obj) OBJECT_CHECK(sifiveSPI, (obj), TYPE_SIFIVE_SPI)
+
+#define R_SCKDIV            (0x00 / 4) /* Serial clock divisor */
+#define R_SCKMODE           (0x04 / 4) /* Serial clock mode */
+#define R_CSID              (0x10 / 4) /* Chip select ID */
+#define R_CSDEF             (0x14 / 4) /* Chip select default */
+#define R_CSMODE            (0x18 / 4) /* Chip select mode */
+#define R_DELAY0            (0x28 / 4) /* Delay control 0 */
+#define R_DELAY1            (0x2C / 4) /* Delay control 1 */
+#define R_FMT               (0x40 / 4) /* Frame format */
+#define R_TXDATA            (0x48 / 4) /* Tx FIFO data */
+#define R_RXDATA            (0x4C / 4) /* Rx FIFO data */
+#define R_TXMARK            (0x50 / 4) /* Tx FIFO watermark */
+#define R_RXMARK            (0x54 / 4) /* Rx FIFO watermark */
+#define R_FCTRL             (0x60 / 4) /* SPI flash interface control */
+#define R_FFMT              (0x64 / 4) /* SPI flash instruction format */
+#define R_IE                (0x70 / 4) /* Interrupt Enable Register */
+#define R_IP                (0x74 / 4) /* Interrupt Pendings Register */
+#define R_MAX               (0x78 / 4)
+
+/* ie and ip bits */
+#define SIFIVE_SPI_IP_TXWM               BIT(0)
+#define SIFIVE_SPI_IP_RXWM               BIT(1)
+
+#define SIFIVE_SPI_RXDATA_DATA_MASK      0xffU
+
+typedef struct sifiveSPI {
+    SysBusDevice parent_obj;
+
+    MemoryRegion mmio;
+
+    qemu_irq irq;
+    int irqline;
+
+    uint8_t num_cs;
+    qemu_irq *cs_lines;
+
+    SSIBus *ssi;
+
+    uint32_t regs[R_MAX];
+} sifiveSPI;
+
+#endif
